c7fb0b045273d40884fe8e36991a3d56123bd725
Laura Tammpere <laura.tammpere@gmail.com>
2019-11-14 12:22:33 -0800

    experimentign with 2htdp/universe

---
+++ universe/cactus-world.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; Set up UI
+(define WIDTH 500)
+(define HEIGHT 500)
+
+(define BACKDROP (rectangle WIDTH HEIGHT "solid" "goldenrod"))
+(define CACTUS-IMG (bitmap/file (string->path "cactus.png")))
+
+; Set up cactus 
+(struct cactus (x-pos y-pos))
+(define init-cactus (cactus (/ WIDTH 2) (/ HEIGHT 2)))
+
+; Set up world
+(struct world (cactus))
+(define init-world (world init-cactus))
+
+
+; Draw function
+(define (render world)
+  (let ([cactus (world-cactus world)])
+  (place-image CACTUS-IMG
+               (cactus-x-pos cactus)
+               (cactus-y-pos cactus)
+               BACKDROP)))
+
+
+; Run the world!
+(define (main w)
+  (big-bang w
+    (to-draw render)))
+
+(main init-world)
